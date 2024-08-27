776ce2fe6159f6abf6c7ee46a26017a73b02f0b2
Fernando9200 <fernando91.sosa@hotmail.com>
2024-08-27 11:51:35 -0300

    Improved responsibility in smaller devices

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import './Navbar.css';
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(prevState => !prevState);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo"></div>
-      <ul className="navbar-links">
-        <li><Link to="landing" smooth={true} duration={500}>Home</Link></li>
-        <li><Link to="about" smooth={true} duration={500}>About</Link></li>
-        <li><Link to="portfolio" smooth={true} duration={500}>Portfolio</Link></li>
-        <li><Link to="timeline" smooth={true} duration={500}>Timeline</Link></li>
+      <ul className={`navbar-links ${isOpen ? 'navbar-links-mobile' : ''}`}>
+        <li><Link to="landing" smooth={true} duration={500} onClick={closeMenu}>Home</Link></li>
+        <li><Link to="about" smooth={true} duration={500} onClick={closeMenu}>About</Link></li>
+        <li><Link to="portfolio" smooth={true} duration={500} onClick={closeMenu}>Portfolio</Link></li>
+        <li><Link to="timeline" smooth={true} duration={500} onClick={closeMenu}>Timeline</Link></li>
       </ul>
+      <div className="hamburger" onClick={toggleMenu}>
+        <div />
+        <div />
+        <div />
+      </div>
     </nav>
   );
 }
